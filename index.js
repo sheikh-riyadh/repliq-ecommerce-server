3ec4ad740eac1d0ba6af2c6091fbd3b6622b195b
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-01-21 14:34:43 +0600

    connect database and setup environment variable

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -9,7 +11,29 @@ app.use(express.json())
 
 
 
-app.get('/', (req, res) => {
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wjboujk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        const productsCollection = client.db('repliq-ecommerce').collection('products')
+        app.get('/', async (req, res) => {
+            const query = {}
+            const products = await productsCollection.find(query).toArray()
+            res.send(products)
+        })
+    } finally {
+
+    }
+}
+run().catch(e => console.error(e))
+
+
+
+
+app.get('/', async (req, res) => {
     res.send("Hey developer i am calling from server");
 })
 app.listen(port, () => {
