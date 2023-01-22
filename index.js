339a1342db6eabc12690a29471a7518016d5c951
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-01-23 00:18:19 +0600

    created user data store api and store data

--- index.js
+++ index.js
@@ -20,38 +20,50 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const run = async () => {
     try {
         /* All Collections here */
-        const productsCollection = client.db('repliq-ecommerce').collection('products')
-        const addToCartCollection = client.db('repliq-ecommerce').collection('add-to-cart')
+        const productsCollection = client.db('repliq-ecommerce').collection('products');
+        const addToCartCollection = client.db('repliq-ecommerce').collection('add-to-cart');
+        const usersCollection = client.db('repliq-ecommerce').collection('users-collection');
 
 
         /* Get all products from here */
         app.get('/', async (req, res) => {
-            const query = {}
-            const products = await productsCollection.find(query).toArray()
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
             res.send(products);
         });
 
         /* Get all cart product from here */
         app.get('/add-to-cart', async (req, res) => {
-            const email = req.query.email
-            const query = { buyerEmail: email }
-            const addToCartProducts = await addToCartCollection.find(query).toArray()
-            res.send(addToCartProducts)
+            const email = req.query.email;
+            const query = { buyerEmail: email };
+            const addToCartProducts = await addToCartCollection.find(query).toArray();
+            res.send(addToCartProducts);
         })
 
         /* Save purchase product */
         app.post('/add-to-cart', async (req, res) => {
             const product = req.body;
             const result = await addToCartCollection.insertOne(product);
-            res.send(result)
+            res.send(result);
         });
+
+        /* Save user from here */
+        app.post('/save-user', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+
+        })
 
         /* Delete single cart product here */
         app.delete('/add-to-cart/:id', async (req, res) => {
-            query = { _id: ObjectId(req.params.id) }
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
             const result = await addToCartCollection.deleteOne(query)
             res.send(result)
         })
+
+
 
 
 
